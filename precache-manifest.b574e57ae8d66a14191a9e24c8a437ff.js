6cc155d957e679acd8f6814761b1a527a8cf1819
jorge felipe cunha <jorgec@alunos.utfpr.edu.br>
2020-09-30 19:29:27 -0300

    Updates

---
+++ precache-manifest.b574e57ae8d66a14191a9e24c8a437ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abda1705b47dbb9a2ba351538fb0fc5f",
+    "url": "/Projeto-Web-Dois/index.html"
+  },
+  {
+    "revision": "8c0a4721dea200130412",
+    "url": "/Projeto-Web-Dois/static/css/2.4a6f958a.chunk.css"
+  },
+  {
+    "revision": "8c0a4721dea200130412",
+    "url": "/Projeto-Web-Dois/static/js/2.dd64b930.chunk.js"
+  },
+  {
+    "revision": "660e47f442ba59f95a5b14bffb690494",
+    "url": "/Projeto-Web-Dois/static/js/2.dd64b930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e760ae23fe51eb59dc",
+    "url": "/Projeto-Web-Dois/static/js/main.a9688c5d.chunk.js"
+  },
+  {
+    "revision": "77a9bb9fc25fd117594e",
+    "url": "/Projeto-Web-Dois/static/js/runtime-main.4bd0a2de.js"
+  },
+  {
+    "revision": "8d86fc0fcd7672d234ec55e437a6f305",
+    "url": "/Projeto-Web-Dois/static/media/fundooo.8d86fc0f.jpg"
+  },
+  {
+    "revision": "5260bac0dc43013f3ab4539fba687ff3",
+    "url": "/Projeto-Web-Dois/static/media/react.5260bac0.png"
+  }
+]);
